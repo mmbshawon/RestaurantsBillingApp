ad2724129d3f3e960b4ef5d31f52eea6438ded87
mmbshawon <masum27522@gmail.com>
2019-11-13 22:15:33 +0600

    add all my files

---
+++ RestaurantsBilling.Model/FoodItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantsBilling.Model
+{
+    public class FoodItem
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public int ItemPrice { get; set; }
+        public int ItemNumber { get; set; }
+        public string ItemCategory { get; set; }
+    }
+}
